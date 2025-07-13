65e6d1df674b38beb6bebf2d820db42ec68e2b83
Nikhil-Dev <hi.nikhil25@gmail.com>
2025-07-13 23:25:14 +0530

    circuit breaker implementation

--- src/main/java/com/smartcoder/OrderService/external/client/PaymentService.java
+++ src/main/java/com/smartcoder/OrderService/external/client/PaymentService.java
@@ -1,6 +1,8 @@
 package com.smartcoder.OrderService.external.client;
 
+import com.smartcoder.OrderService.exception.CustomException;
 import com.smartcoder.OrderService.external.request.PaymentRequest;
+import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -10,10 +12,14 @@ import org.springframework.web.bind.annotation.RequestBody;
     * This interface is used to communicate with the Payment Service.
     * It will be called by the Order Service.
  */
+@CircuitBreaker(name = "external", fallbackMethod = "fallback")
 @FeignClient(name = "PAYMENT-SERVICE/payment")
 public interface PaymentService {
 
     @PostMapping
     ResponseEntity<Long> doPayment(@RequestBody PaymentRequest paymentRequest);
 
+    default void fallback(Exception e) {
+        throw  new CustomException("Payment Service is not available", "UNAVAILABLE", 500);
+    }
 }
--- src/main/java/com/smartcoder/OrderService/external/client/ProductService.java
+++ src/main/java/com/smartcoder/OrderService/external/client/ProductService.java
@@ -1,5 +1,7 @@
 package com.smartcoder.OrderService.external.client;
 
+import com.smartcoder.OrderService.exception.CustomException;
+import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -11,6 +13,7 @@ import org.springframework.web.bind.annotation.RequestParam;
     * This interface is used to communicate with the Product Service.
     * It will be called by the Order Service.
  */
+@CircuitBreaker(name = "external", fallbackMethod = "fallback")
 @FeignClient(name = "PRODUCT-SERVICE/product")
 public interface ProductService {
 
@@ -19,4 +22,8 @@ public interface ProductService {
             @PathVariable("id") long productId,
             @RequestParam long quantity
     );
+
+    default void fallback(Exception e) {
+        throw  new CustomException("Product Service is not available", "UNAVAILABLE", 500);
+    }
 }
